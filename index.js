8e4c953eb5f7f87a2cbafbd9d1d5c3a7ee7cafa4
andyjansson <andyjansson@users.noreply.github.com>
2016-01-12 17:59:06 +0100

    v2 rewrite

--- index.js
+++ index.js
@@ -1,188 +1,155 @@
 var postcss = require('postcss'),
-    vars = require('postcss-simple-vars'),
-    path = require('path'),
-    fs = require('fs'),
-    glob = require('glob');
+	fs = require('fs'),
+	path = require('path'),
+	glob = require('glob'),
+	vars = require('postcss-simple-vars');
 
-var stringToAtRule = function (str, obj) {
-    obj.name   = str.match(/^@([^\s]*)/)[1];
-    obj.params = str.replace(/^@[^\s]*\s+/, '');
-    return obj;
+var parseNameAndArgs = function (rule) {
+	var name = rule.params.split('(', 1)[0],
+		rest = rule.params.slice(name.length).trim(),
+		args;
+
+	if (rest.length) {
+		if (
+			rest.substring(0, 1) !== '(' ||
+			rest.substring(rest.length - 1) !== ')'
+			) {
+			throw rule.error(
+				'Syntax error',
+				{ plugin: 'postcss-sassy-mixins' }
+			);
+		}
+		args = postcss.list.comma(rest.substring(1, rest.length - 1));
+	}
+
+	return {
+		name: name,
+		args: args
+	};
 };
 
 var objectToNodes = function (node, obj, source) {
-    var name, value, decl, rule;
-    for ( name in obj ) {
-        value = obj[name];
-        if ( typeof value === 'object' ) {
-            if ( name[0] === '@' ) {
-                rule = postcss.atRule(stringToAtRule(name, { source: source }));
-            } else {
-                rule = postcss.rule({ selector: name, source: source });
-            }
-            node.append(rule);
-            if ( typeof value === 'object' ) objectToNodes(rule, value, source);
-        } else {
-            decl = postcss.decl({
-                prop:   name,
-                value:  value.toString(),
-                source: source
-            });
-            node.append(decl);
-        }
-    }
-    return node;
-};
-
-var insertObject = function (rule, obj) {
-    var root = objectToNodes(postcss.root(), obj, rule.source);
-    rule.parent.insertBefore(rule, root);
-};
-
-var parseNameAndArgs = function (str, rule) {
-    var name = str.split('(', 1)[0],
-        rest = str.slice(name.length).trim(),
-        args;
-
-    if (rest.length) {
-        if (
-            rest.substring(0, 1) !== '(' ||
-            rest.substring(rest.length - 1) !== ')'
-            ) {
-            throw rule.error(
-                'Syntax error',
-                { plugin: 'postcss-sassy-mixins' }
-            );
-        }
-        args = rest.substring(1, rest.length - 1);
-    }
-
-    return {
-        name: name,
-        args: args
-    };
-};
-
-var defineMixin = function (result, mixins, rule) {
-    var parsed = parseNameAndArgs(rule.params, rule),
-        args = [];
-
-    if (parsed.args) {
-        args = postcss.list.comma(parsed.args).map(function(str) {
-            var arg = str.split(':', 1)[0];
-            var defaults = str.slice(arg.length + 1);
-            return [arg.slice(1).trim(), defaults.trim()];
-        });
-    }
-
-    var content = false;
-    rule.eachAtRule('content', function () {
-        content = true;
-        return false;
-    });
-
-    mixins[parsed.name] = { mixin: rule, args: args, content: content };
-    rule.removeSelf();
-};
-
-var insertMixin = function (result, mixins, rule, opts) {
-    var parsed = parseNameAndArgs(rule.params, rule),
-        params = [];
-
-    if (parsed.args) {
-        params = postcss.list.comma(parsed.args);
-    }
-
-    var def = mixins[parsed.name];
-    var mixin = def && def.mixin;
-
-    if (!def) {
-        if ( !opts.silent ) {
-            throw rule.error('Undefined mixin ' + parsed.name);
-        }
-    }
-    else if (mixin.name === 'mixin') {
-        var values = { };
-        for ( var i = 0; i < def.args.length; i++ ) {
-            values[ def.args[i][0] ] = params[i] || def.args[i][1];
-        }
-
-        var clones = [];
-        for ( i = 0; i < mixin.nodes.length; i++ ) {
-            clones.push( mixin.nodes[i].clone() );
-        }
-
-        var proxy = postcss.rule({ nodes: clones });
-        if ( def.args.length ) {
-            vars({ only: values })(proxy);
-        }
-
-        if ( def.content ) {
-            proxy.eachAtRule('content', function (place) {
-                place.replaceWith(rule.nodes);
-            });
-        }
-        rule.parent.insertBefore(rule, clones);
-    }
-    else if ( typeof mixin === 'object' ) {
-        insertObject(rule, mixin, rule.source);
-    }
-    else if ( typeof mixin === 'function' ) {
-        var args  = [rule].concat(params);
-        var nodes = mixin.apply(this, args);
-        if ( typeof nodes === 'object' ) {
-            insertObject(rule, nodes, rule.source);
-        }
-    }
-
-    if ( rule.parent ) rule.removeSelf();
+	Object.keys(obj).forEach(function(key) {
+		if (typeof obj[key] === 'object') {
+			var rule;
+			if (key[0] === '@')
+				rule = postcss.atRule({
+					name: key.match(/^@([^\s]*)/)[1],
+					params: key.replace(/^@[^\s]*\s+/, '')
+				});
+			else
+				rule = postcss.rule({ selector: key, source: source });
+			node.append(rule);
+			objectToNodes(rule, obj[key], source);
+		}
+		else {
+			var decl = postcss.decl({
+				prop: key,
+				value: obj[key].toString(),
+				source: source
+			});
+			node.append(decl);
+		}
+	});
+	return node;
 };
 
 module.exports = postcss.plugin('postcss-sassy-mixins', function (opts) {
-    if ( typeof opts === 'undefined' ) opts = { };
+	opts = opts || {};
+	var mixins = {};
+	if (opts.mixinsDir) {
+		var dirs = opts.mixinsDir;
+		if (!(dirs instanceof Array)) dirs = [dirs];
+		dirs.forEach(function(dir) {
+			var files = fs.readdirSync(dir);
+			files.forEach(function(filename) {
+				var file = path.join(dir, filename);
+				if (path.extname(file) !== '.js') return;
+				var name = path.basename(file, '.js');
+				mixins[name] = { mixin: require(file) };
+			});
+		});
+	}
 
-    var i;
-    var mixins = { };
+	if (opts.mixinsFiles) {
+		var globs = opts.mixinsFiles;
+		if (!(globs instanceof Array)) globs = [globs];
+		globs.forEach(function(pattern) {
+			glob.sync(pattern).forEach(function(file) {
+				var name = path.basename(file, path.extname(file));
+				mixins[name] = { mixin: require(file) };
+			});
+		});
+	}
 
-    if ( opts.mixinsDir ) {
-        var dirs = opts.mixinsDir;
-        if ( !(dirs instanceof Array) ) dirs = [dirs];
+	if (typeof opts.mixins === 'object') {
+		Object.keys(opts.mixins).forEach(function(name) {
+			mixins[name] = { mixin: opts.mixins[name] };
+		});
+	}
 
-        for ( i = 0; i < dirs.length; i++ ) {
-            var dir   = dirs[i];
-            var files = fs.readdirSync(dir);
-            for ( var j = 0; j < files.length; j++ ) {
-                var file = path.join(dir, files[j]);
-                if ( path.extname(file) === '.js' ) {
-                    var name = path.basename(file, '.js');
-                    mixins[name] = { mixin: require(file) };
-                }
-            }
-        }
-    }
+	var defineMixin = function (rule) {
+		var parsed = parseNameAndArgs(rule),
+			args;
 
-    if ( opts.mixinsFiles ) {
-        var globs = opts.mixinsFiles;
-        if ( !(globs instanceof Array) ) globs = [globs];
+		if (parsed.args)
+			args = parsed.args.map(function(str) {
+				var arg = str.split(':', 1)[0];
+				var defaults = str.slice(arg.length + 1);
+				return [arg.slice(1).trim(), defaults.trim()];
+			});
 
-        globs.forEach(function(pattern) {
-            glob.sync(pattern).forEach(function(file2) {
-                var name2 = path.basename(file2, path.extname(file2));
-                mixins[name2] = { mixin: require(file2) };
-            });
-        });
-    }
+		var content = false;
+		rule.walkAtRules('content', function () {
+			content = true;
+			return false;
+		});
 
-    if ( typeof opts.mixins === 'object' ) {
-        for ( i in opts.mixins ) mixins[i] = { mixin: opts.mixins[i] };
-    }
+		mixins[parsed.name] = { mixin: rule, args: args, content: content };
+		rule.remove();
+	};
 
-    return function (css, result) {
-        css.eachAtRule('mixin', function (rule) {
-            defineMixin(result, mixins, rule);
-        });
-        css.eachAtRule('include', function (rule) {
-            insertMixin(result, mixins, rule, opts);
-        });
-    };
+	var includeMixin = function (rule) {
+		var parsed = parseNameAndArgs(rule);
+		var decl = mixins[parsed.name];
+		if (!decl && !opts.silent)
+			throw rule.error('Undefined mixin ' + parsed.name);
+		var mixin = decl && decl.mixin;
+		var params = parsed.args || [];
+		if (typeof mixin === 'function') {
+			var args = [rule].concat(params);
+			mixin = mixin.apply(this, args);
+		}
+		if (typeof mixin === 'object') {
+			if (mixin.type && mixin.name &&
+				mixin.type === 'atrule' && mixin.name === 'mixin') {
+				var values = {};
+				if (decl.args) decl.args.forEach(function (arg, i) {
+					values[arg[0]] = params[i] || arg[1];
+				});
+				var clones = [];
+				mixin.nodes.forEach(function(node) {
+					clones.push(node.clone());
+				});
+				var proxy = postcss.rule({ nodes: clones });
+				if (decl.args) vars({ only: values })(proxy);
+				if (decl.content) {
+					proxy.walkAtRules('content', function (place) {
+						place.replaceWith(rule.nodes);
+					});
+				}
+				rule.parent.insertBefore(rule, proxy.nodes);
+				proxy.walkAtRules('include', includeMixin);
+			}
+			else {
+				var root = objectToNodes(postcss.root(), mixin, rule.source);
+				rule.parent.insertBefore(rule, root);
+			}
+		}
+		rule.remove();
+	};
+	return function (css) {
+		css.walkAtRules('mixin', defineMixin);
+		css.walkAtRules('include', includeMixin);
+	};
 });
